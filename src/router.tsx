8bf609b34c89eb7113a8f76cf885cc949fbd10e3
Kelvin <kieyg7@gmail.com>
2023-02-15 22:36:26 +0900

    day-10

--- src/router.tsx
+++ src/router.tsx
@@ -2,10 +2,10 @@ import {createBrowserRouter} from "react-router-dom";
 import Root from "./Root";
 import ToDoList from "./components/TodoList";
 
-// const BASE_URL = '';
+const BASE_URL = 'https://kieyg7.github.io/react-day10-todo';
 const router = createBrowserRouter([
   {
-    path: '',
+    path: BASE_URL,
     element: <Root />,
     children: [
       {
